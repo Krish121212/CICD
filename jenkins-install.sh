6cf7c91050cd40f9b52800323246639ebdf78a8b
surya pelluru <maladikrishnakishore@gmail.com>
2024-06-10 00:44:57 +0530

    ec2-jenkins install

---
+++ jenkins-install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo "[jenkins]
+name=Jenkins-stable
+baseurl=http://pkg.jenkins.io/redhat-stable
+gpgcheck=1" > /etc/yum.repos.d/jenkins.repo
+
+cd /etc/yum.repos.d/
+
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key 
+sudo dnf install fontconfig java-17-openjdk 
+sudo dnf install jenkins 
+systemctl daemon-reload 
+systemctl enable jenkins
+systemctl start jenkins
+systemctl status jenkins
