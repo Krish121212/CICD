7564234f94aaa9fc17a3a13cd3c2fc5382626e55
surya pelluru <maladikrishnakishore@gmail.com>
2024-06-10 23:38:47 +0530

    install-jenkins

--- instance-creation/data.tf
+++ instance-creation/data.tf
@@ -7,15 +7,11 @@ data "aws_ami" "ami-info" {
     values = ["RHEL-9-DevOps-*"]
   }
   filter {
-    name   = "Root device type"
+    name   = "root-device-type"
     values = ["ebs"]
   }
   filter {
-    name   = "Owner account ID"
-    values = ["973714476881"]
-  }
-  filter {
-    name   = "Virtualization type"
+    name   = "virtualization-type"
     values = ["hvm"]
   }
 }
